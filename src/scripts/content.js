41bbe44d90eb8fd9a3b817cc8848d7b27f928e11
Duhoon <eclip6@naver.com>
2022-12-18 16:11:50 +0900

    feat: modal view for git commit message

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -2,8 +2,6 @@ const makeGitHubBtn = ()=>{
   const url = new URL(window.location.href);
 
   if (!url.pathname.match(/codeproblem/)) return;
-
-  const problemHash = url.pathname.split('/')[2];
   
   const btnGroup = document.querySelector('div.item.end');
 
@@ -20,71 +18,94 @@ const makeGitHubBtn = ()=>{
       alert('테스트 후에 커밋하세요!');
       return;
     }
-    const fileName = `${document.querySelector('span.problem-title').textContent}.js`;
-    const fileContent = localStorage.getItem(problemHash);
-
-    // TODO 커밋 메시지 입력 가능하게 만든다.
-    const commitMessage = '';
-    
-    // // TEST명령줄 (나중에 지우세요)
-    // const modalContainer = document.querySelector(".modal-container")
-    // modalContainer.classList.toggle("activate");
-
-    alert('해당 코드를 커밋합니다.');
-    chrome.runtime.sendMessage({
-      action: 'Commit', fileName, fileContent, commitMessage,
-    }, (response) => {
-      alert(response.message);
-      alert('커밋 완료!');
-    });
+  
+    makeGitHubModal();
   })
 }
 
+// ==============================
+//            Modal
+// ==============================
 
 const makeGitHubModal = ()=>{
-  const modalContainer = document.createElement("div");
-  const modal = document.createElement("div");
-  const commitMessageInput = document.createElement("input");
-  const btnGroup = document.createElement("div");
-  const cancelBtn = document.createElement("button");
-  const commitBtn = document.createElement("button");
+  const gitModalInnerHTML =
+  `
+    <div class="ant-modal-mask ant-fade-enter ant-fade-enter-active">
+      <div class="ant-modal-wrap activate">
+        <div class="ant-modal">
+          <div class="ant-modal-content">
+            <div class="ant-modal-header">
+              <div class="ant-modal-title">커밋하기</div>
+            </div>
+            <div class="ant-modal-body">
+              <input class="commit-message-input" name="commit-message" placeholder="커밋 메시지를 입력해주세요.">
+              <div class="btn-group">
+                <button class="ant-btn btn btn-cancel">취소</button>
+                <button class="ant-btn btn btn-primary btn-commit">커밋</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `
 
-  // Commit Message Input
-  commitMessageInput.className = "commit-message-input";
-  commitMessageInput.name = "commit-message";
-  commitMessageInput.placeholder = "커밋 메시지를 입력해주세요.";
+  const gitModal = document.createElement("div");
+  gitModal.className="git-coplit-modal";
+  gitModal.innerHTML= gitModalInnerHTML;
+  document.body.prepend(gitModal);
 
-  // Buttons
-  btnGroup.className = "btn-group";
+  const modalMask = document.querySelector(".git-coplit-modal .ant-modal-mask");
+  const modalWrap = document.querySelector(".git-coplit-modal .ant-modal-wrap");
 
-  cancelBtn.className = "btn";
-  cancelBtn.textContent = "취소";
+  const commitMessageInput = document.querySelector(".git-coplit-modal .commit-message-input");
+  const cancelBtn = document.querySelector(".git-coplit-modal .btn-cancel");
+  const commitBtn = document.querySelector(".git-coplit-modal .btn-commit");
 
-  commitBtn.className = "btn btn-primary"
-  commitBtn.textContent = "커밋";
+  // Function Remove modal
+  const removeModal = ()=>{
+    modalMask.classList.toggle("ant-fade-enter");
+    modalMask.classList.toggle("ant-fade-enter-active");
+    modalMask.classList.toggle("ant-fade-leave");
+    modalMask.classList.toggle("ant-fade-leave-active");
+    modalMask.classList.toggle("active");
+    initInput();
+    setTimeout(()=>{
+      document.body.removeChild(gitModal);
+    }, 600);
+  }
 
-  btnGroup.append(cancelBtn, commitBtn);
-
-  // Modal
-  modalContainer.className = "modal-container";
-  modal.className= "modal";
-
-  modal.append(commitMessageInput, btnGroup);
-  modalContainer.append(modal);
-
-  // Inject modal to document;
-  document.body.prepend(modalContainer);
-
-
-  // Handlers
+  // Handler
   const initInput = (input)=>{
     commitMessageInput.value = "";
   }
 
-  cancelBtn.addEventListener("click", ()=>{
-    modalContainer.classList.toggle("activate");
-    initInput();
-  })
+  const cancelBtnHandler = ()=>{
+    removeModal();
+  }
+
+  const commitBtnHandler = async()=>{
+    const url = new URL(window.location.href);
+
+    const problemHash = url.pathname.split('/')[2];
+
+    const fileName = `${document.querySelector('span.problem-title').textContent}.js`;
+    const fileContent = localStorage.getItem(problemHash);
+
+    const commitMessage = commitMessageInput.value;
+
+    alert('해당 코드를 커밋합니다.');
+    await chrome.runtime.sendMessage({
+      action: 'Commit', fileName, fileContent, commitMessage,
+    }, (response) => {
+      alert(response.message);
+      removeModal();
+    });
+  }
+
+  // Register EventListener
+  cancelBtn.addEventListener("click", cancelBtnHandler);
+  commitBtn.addEventListener("click", commitBtnHandler);
 }
 
 
@@ -103,7 +124,5 @@ window.addEventListener("locationchange", ()=>{
     makeGitHubBtn();
   },0);
 })
-
-makeGitHubModal();
 
 // https://stackoverflow.com/questions/3522090/event-when-window-location-href-changes
